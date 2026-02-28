94f669974a11e6ca943ef52d5f26681d29f44b40
Troy Spjute <tarogstar@gmail.com>
2026-02-28 11:34:46 -0700

    fix: remove stale ReplyRuntimeConfigSchemaShape re-export from plugin-sdk

--- src/plugin-sdk/index.ts
+++ src/plugin-sdk/index.ts
@@ -197,7 +197,6 @@ export {
   MarkdownConfigSchema,
   MarkdownTableModeSchema,
   normalizeAllowFrom,
-  ReplyRuntimeConfigSchemaShape,
   requireOpenAllowFrom,
   TtsAutoSchema,
   TtsConfigSchema,
